7167165349d2b6fd3d9ed2375ef13057397913a1
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2015-06-05 16:37:43 +0200

    MAINT: fixing accent in my name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='tmscoring', version='0.1',
       description='Python implementation of the TMscore program',
       url='',
-      author='David Menéndez Hurtado',
+      author='David Men\'endez Hurtado',
       license='BSD 3-clause',
       packages=['tmscoring'],
       requires=['numpy', 'iminuit', 'biopython'],
