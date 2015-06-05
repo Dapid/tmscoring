09451e3379379badf115925a425018295f4fbf11
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2015-06-05 16:40:20 +0200

    TST: fixing tests

--- tmscoring/tests/test.py
+++ tmscoring/tests/test.py
@@ -2,13 +2,12 @@ from __future__ import division
 import tmscoring
 
 import numpy as np
-from numpy.testing import (assert_equal, assert_raises, assert_almost_equal,
-                           assert_array_equal, TestCase, run_module_suite)
+from numpy.testing import assert_almost_equal, TestCase
 
 
 class TestAligningBase(TestCase):
     def test_matrix(self):
-        align_object = tmscoring.Aligning('pdb1', 'pdb2')
+        align_object = tmscoring.Aligning('pdb1.pdb', 'pdb2.pdb')
         np.random.seed(124)
         for _ in xrange(100):
             theta, phi, psi = 2 * np.pi * np.random.random(3)
@@ -20,7 +19,7 @@ class TestAligningBase(TestCase):
             assert_almost_equal(1, np.linalg.det(matrix), 6)
 
     def test_tm_valuex(self):
-        align_object = tmscoring.Aligning('pdb1', 'pdb2')
+        align_object = tmscoring.Aligning('pdb1.pdb', 'pdb2.pdb')
         np.random.seed(124)
         for _ in xrange(100):
             theta, phi, psi = 2 * np.pi * np.random.random(3)
@@ -31,13 +30,13 @@ class TestAligningBase(TestCase):
             assert 0 <= -tm / align_object.N <= 1
 
     def test_load_data_alignment(self):
-        align_object = tmscoring.Aligning('pdb1', 'pdb2', mode='align')
+        align_object = tmscoring.Aligning('pdb1.pdb', 'pdb2.pdb', mode='align')
         assert align_object.coord1.shape[0] == 4
         assert align_object.coord2.shape[0] == 4
         assert align_object.coord1.shape == align_object.coord2.shape
 
     def test_load_data_index(self):
-        align_object = tmscoring.Aligning('pdb1', 'pdb2', mode='index')
+        align_object = tmscoring.Aligning('pdb1.pdb', 'pdb2.pdb', mode='index')
         assert align_object.coord1.shape[0] == 4
         assert align_object.coord2.shape[0] == 4
         assert align_object.coord1.shape == align_object.coord2.shape
