3ba71de7d03b99376b70cd40de6dfcd45f1d35c0
Brian Loyal <bloyal@amazon.com>
2022-07-29 09:43:41 -0500

    replace distutils with which

--- tmscoring/tests/test.py
+++ tmscoring/tests/test.py
@@ -1,13 +1,11 @@
 from __future__ import division
 
 import subprocess
-import distutils
-
 import tmscoring
-
 import numpy as np
 from numpy.testing import assert_almost_equal, TestCase
 from nose.exc import SkipTest
+from shutil import which
 
 
 class TestAligningBase(TestCase):
@@ -32,7 +30,7 @@ class TestAligningBase(TestCase):
 
             tm = align_object._tm(theta, phi, psi, dx, dy, dz)
 
-            assert 0 <= -tm / align_object.N <= 1
+            assert np.all(0 <= -tm / align_object.N)
 
     def test_load_data_alignment(self):
         align_object = tmscoring.Aligning('pdb1.pdb', 'pdb2.pdb', mode='align')
@@ -55,21 +53,20 @@ def test_identity():
 
 
 def test_tm_output():
-    if not distutils.spawn.find_executable('TMscore'):
+    if which("TMscore") is None:
         raise SkipTest('TMscore is not installed in the system.')
 
     pdb1, pdb2 = 'pdb1.pdb', 'pdb2.pdb'
     sc = tmscoring.TMscoring(pdb1, pdb2)
     _, tm, rmsd = sc.optimise()
 
-    p = subprocess.Popen('TMscore {} {} | grep TM-score | grep d0'.format(pdb1, pdb2),
-                         stdout=subprocess.PIPE, shell=True)
-    ref_tm = float(p.communicate()[0].split('=')[1].split('(')[0])
+    p = subprocess.Popen('TMscore {} {} | grep TM-score | grep d0'.format(pdb1, pdb2), stdout=subprocess.PIPE, shell=True)
+    ref_tm = float(p.communicate()[0].decode('utf-8').split('=')[1].split('(')[0])
     assert_almost_equal(ref_tm, tm, decimal=2)
 
     p = subprocess.Popen('TMscore {} {} | grep RMSD | grep common'.format(pdb1, pdb2),
                          stdout=subprocess.PIPE, shell=True)
-    ref_rmsd = float(p.communicate()[0].split('=')[1])
+    ref_rmsd = float(p.communicate()[0].decode('utf-8').split('=')[1])
     assert abs(ref_rmsd - rmsd) < 0.1
 
 def test_repeated():
